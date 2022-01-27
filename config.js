66b8e5ca132d76f82da1c69e7dbcf821a31b47ee
bovtik <bovtik.work@gmail.com>
2022-01-28 01:36:10 +0200

    add super rare hyperBubbles

--- config.js
+++ config.js
@@ -9,4 +9,7 @@ if (rand <= 0.1) {
   CONFIG.bubbleGarden = true;
 } else if (rand > 0.1 && rand <= 0.2) {
   CONFIG.hyperOrbits = true;
+} else if (rand > 0.2 && rand <= 0.23) {
+  CONFIG.bubbleGarden = true;
+  CONFIG.hyperOrbits = true;
 }
